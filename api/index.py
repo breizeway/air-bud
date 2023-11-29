16e932f4ca1e622e1be1cbbed3ae621a1599358a
Tannor Breitigam <70126993+breizeway@users.noreply.github.com>
2023-11-29 15:15:48 -0800

    imports 4

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from ariadne import graphql_sync, make_executable_schema, load_schema_from_path
 from ariadne.explorer import ExplorerGraphiQL
 from flask import Flask, jsonify, request
-from _queries import queries
+from queries import queries
 
 type_defs = load_schema_from_path("api/schema.graphql")
 
