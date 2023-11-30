50e3ac7fb30447491ab6bb66e83a650891437da2
Tannor Breitigam <70126993+breizeway@users.noreply.github.com>
2023-11-29 16:19:41 -0800

    cleanup

--- api/handler.py
+++ api/handler.py
@@ -9,29 +9,18 @@ schema = make_executable_schema(type_defs, [*queries])
 
 app = Flask(__name__)
 
-# Retrieve HTML for the GraphiQL.
-# If explorer implements logic dependant on current request,
-# change the html(None) call to the html(request)
-# and move this line to the graphql_explorer function.
 explorer_html = ExplorerGraphiQL().html(None)
 
 
 @app.route("/graphql", methods=["GET"])
 def graphql_explorer():
-    # On GET request serve the GraphQL explorer.
-    # You don't have to provide the explorer if you don't want to
-    # but keep on mind this will not prohibit clients from
-    # exploring your API using desktop GraphQL explorer app.
     return explorer_html, 200
 
 
 @app.route("/graphql", methods=["POST"])
 def graphql_server():
-    # GraphQL queries are always sent as POST
     data = request.get_json()
 
-    # Note: Passing the request to the context is optional.
-    # In Flask, the current request is always accessible as flask.request
     success, result = graphql_sync(
         schema,
         data,
