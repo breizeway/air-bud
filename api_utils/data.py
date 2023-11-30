ae2892c97f7e3837a303e23aec985610186f975b
Tannor Breitigam <70126993+breizeway@users.noreply.github.com>
2023-11-30 14:20:41 -0800

    box score fully working

--- api_utils/data.py
+++ api_utils/data.py
@@ -1,28 +1,39 @@
+import math
 from espn_api.basketball.box_score import BoxScore, STATS_MAP
 from espn_api.basketball.team import Team
 from espn_api.basketball.box_player import BoxPlayer
 from espn_api.basketball.player import Player
 
 
-def listify_dict(box_stats: STATS_MAP, key_name: str):
+def serialize_dict(box_stats: STATS_MAP, key_name: str):
     result = list()
     for k, v in box_stats.items():
         result.append({key_name: k, **v})
     return result
 
 
+def listify_stats(stats: STATS_MAP):
+    result = list()
+    for k, v in (stats.items() or dict()):
+        val = float(v)
+        result.append(
+            {"category": k, "value": val if math.isfinite(val) else None})
+    return result
+
+
 class ClientPlayerStat:
     def __init__(self, player_stat: STATS_MAP):
         self.scoring_period = player_stat["scoring_period"]
+        self.applied_total = player_stat["applied_total"]
+        self.applied_avg = player_stat["applied_avg"]
         self.team = player_stat["team"]
         self.date = player_stat["date"]
-
-        print(":::::::TOTAL: ", player_stat["total"])
-
-        total = list()
-        for k, v in player_stat["total"]:
-            total.append({"category": k, "value": v})
-        self.total = total
+        if "total" in player_stat and player_stat["total"] is not None:
+            self.total = listify_stats(
+                player_stat["total"])
+        if "avg" in player_stat and player_stat["avg"] is not None:
+            self.avg = listify_stats(
+                player_stat["avg"])
 
 
 class ClientPlayer:
@@ -37,8 +48,8 @@ class ClientPlayer:
         self.injuryStatus = player.injuryStatus
         self.injured = player.injured
         self.stats = map(lambda player_stat: ClientPlayerStat(
-            player_stat), listify_dict(player.stats, "scoring_period"))
-        self.schedule = listify_dict(player.schedule, "scoring_period")
+            player_stat), serialize_dict(player.stats, "scoring_period"))
+        self.schedule = serialize_dict(player.schedule, "scoring_period")
         self.lineupSlot = player.lineupSlot
         self.total_points = player.total_points
         self.avg_points = player.avg_points
@@ -68,9 +79,8 @@ class ClientBoxPlayer:
     def __init__(self, box_player: BoxPlayer):
         self.slot_position = box_player.slot_position
         self.points = box_player.points
-        print(":::POINTs_BREAKDWONS:::", box_player.points_breakdown)
-        # self.points_breakdown = listify_dict(
-        #     box_player.points_breakdown, "category")
+        self.points_breakdown = listify_stats(
+            box_player.points_breakdown)
         self.pro_opponent = box_player.pro_opponent
         self.game_played = box_player.game_played
 
@@ -82,12 +92,12 @@ class ClientBoxScore:
         self.home_wins = box_score.home_wins
         self.home_ties = box_score.home_ties
         self.home_losses = box_score.home_losses
-        self.home_stats = listify_dict(box_score.home_stats, "category")
+        self.home_stats = serialize_dict(box_score.home_stats, "category")
         self.away_team = ClientTeam(box_score.away_team)
         self.away_wins = box_score.away_wins
         self.away_ties = box_score.away_ties
         self.away_losses = box_score.away_losses
-        self.away_stats = listify_dict(box_score.away_stats, "category")
+        self.away_stats = serialize_dict(box_score.away_stats, "category")
         self.home_lineup = map(lambda box_player: ClientBoxPlayer(
             box_player), box_score.home_lineup)
         self.away_lineup = map(lambda box_player: ClientBoxPlayer(
