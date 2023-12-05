5840799f49b64278d102ba7949d618a4b56a0523
Tannor Breitigam <70126993+breizeway@users.noreply.github.com>
2023-12-04 16:15:52 -0800

    callback works

--- serverless/league.py
+++ serverless/league.py
@@ -1,8 +1,24 @@
 from espn_api.basketball import League
+from espn_api.requests.espn_requests import ESPNAccessDenied
+from typing import Callable, Dict, Optional, cast
 import os
+
+
+class ClientError:
+    def __init__(self, code: str, message: str):
+        self.code = code
+        self.message = message
 
 
 def init_league(league_config: dict):
     league_config.update({"year": league_config.get("year", 2024)})
     league_config.update({"league_id": int(os.environ["LEAGUE_ID"])})
     return League(**league_config)
+
+
+def make_league_request(league: League, callback: callable):
+    try:
+        callback(league)
+    except ESPNAccessDenied as err:
+        print(":::ERR::: ", err)
+        return {"client_errors": [ClientError("LEAGUE_AUTH", "League credentials not valid")]}
--- serverless/resolvers/queries.py
+++ serverless/resolvers/queries.py
@@ -14,10 +14,16 @@ class ClientError:
 
 @query.field("getBoxScores")
 def resolve_get_box_scores(*_, league_config):
-    try:
-        league = init_league(league_config)
+    def resolve(callback):
+        try:
+            league = init_league(league_config)
+            return callback(league)
+        except ESPNAccessDenied as err:
+            print(":::ERR::: ", err)
+            return {"client_errors": [ClientError("LEAGUE_AUTH", "League credentials not valid")]}
+
+    def getBoxScores(league):
         box_scores = league.box_scores()
         return {"box_scores": map(lambda box_score: ClientBoxScore(box_score), box_scores)}
-    except ESPNAccessDenied as err:
-        print(":::ERR::: ", err)
-        return {"client_errors": [ClientError("LEAGUE_AUTH", "League credentials not valid")]}
+
+    return resolve(getBoxScores)
