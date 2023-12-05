376ee0fa4bfe012c2eee05e65df924f5af548bfa
Tannor Breitigam <70126993+breizeway@users.noreply.github.com>
2023-12-04 17:20:20 -0800

    add PrivateLeague class with error handling

--- serverless/league.py
+++ serverless/league.py
@@ -1,24 +1,41 @@
 from espn_api.basketball import League
 from espn_api.requests.espn_requests import ESPNAccessDenied
-from typing import Callable, Dict, Optional, cast
+from typing import Callable, Dict
+from enum import Enum
 import os
 
 
+class ClientErrorCode(Enum):
+    LEAGUE_AUTH = "LEAGUE_AUTH"
+    LEAGUE_REQUEST = "LEAGUE_REQUEST"
+
+
 class ClientError:
-    def __init__(self, code: str, message: str):
+    def __init__(self, code: ClientErrorCode, message: str):
         self.code = code
         self.message = message
 
 
-def init_league(league_config: dict):
-    league_config.update({"year": league_config.get("year", 2024)})
-    league_config.update({"league_id": int(os.environ["LEAGUE_ID"])})
-    return League(**league_config)
+class PrivateLeague:
+    def __init__(self, league_config: dict):
+        league_config.update({"year": league_config.get("year", 2024)})
+        league_config.update({"league_id": int(os.environ["LEAGUE_ID"])})
 
+        self.errors = list()
+        try:
+            self.league = League(**league_config)
+        except ESPNAccessDenied:
+            self.league = None
+            self.errors.append(ClientError(
+                ClientErrorCode.LEAGUE_AUTH.value, "League credentials not valid"))
 
-def make_league_request(league_config, callback):
-    try:
-        return callback(init_league(league_config))
-    except ESPNAccessDenied as err:
-        print(":::ERR::: ", err)
-        return {"client_errors": [ClientError("LEAGUE_AUTH", "League credentials not valid")]}
+    def makeRequest(self, callback: Callable[[League], Dict]):
+        if not len(self.errors):
+            try:
+                return callback(self.league)
+            except Exception as err:
+                print("ERR: LEAGUE_REQUEST: ", err)
+                self.errors.append(ClientError(
+                    ClientErrorCode.LEAGUE_REQUEST.value, "There was a problem making a request to the ESPN api"))
+
+        return {"client_errors": self.errors}
--- serverless/resolvers/queries.py
+++ serverless/resolvers/queries.py
@@ -1,15 +1,9 @@
 from ariadne import QueryType
 from data import ClientBoxScore
-from league import init_league, make_league_request
+from league import PrivateLeague
 from espn_api.requests.espn_requests import ESPNAccessDenied
 
 query = QueryType()
-
-
-class ClientError:
-    def __init__(self, code: str, message: str):
-        self.code = code
-        self.message = message
 
 
 @query.field("getBoxScores")
@@ -18,4 +12,4 @@ def resolve_get_box_scores(*_, league_config):
         box_scores = league.box_scores()
         return {"box_scores": map(lambda box_score: ClientBoxScore(box_score), box_scores)}
 
-    return make_league_request(league_config, getBoxScores)
+    return PrivateLeague(league_config).makeRequest(getBoxScores)
