c6d7bf1aaff32fcd160120b5da86fb3b6fa5cd78
Tannor Breitigam <70126993+breizeway@users.noreply.github.com>
2023-12-06 22:32:59 -0800

    wc

--- serverless/resolvers/queries.py
+++ serverless/resolvers/queries.py
@@ -2,6 +2,8 @@ from ariadne import QueryType
 from data import ClientBoxScore
 from resources.league_api import LeagueApi
 import json
+from typing import List
+from espn_api.basketball.box_score import BoxScore
 
 query = QueryType()
 
@@ -9,8 +11,9 @@ query = QueryType()
 @query.field("getBoxScores")
 def resolve_get_box_scores(*_, year=None):
     def getBoxScores(league):
-        box_scores = league.box_scores()
-        print("BOX_SCORES: ", json.dumps(box_scores))
+        box_scores: List[BoxScore] = league.box_scores()
+
+        print("BOX_SCORES: ", json.dumps(box_scores.__dict__))
         return {"box_scores": map(lambda box_score: ClientBoxScore(box_score), box_scores)}
 
     return LeagueApi(year).make_request(getBoxScores)
