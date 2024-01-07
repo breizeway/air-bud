8f66b805d24f56ac87f4ac15af16637774f48111
Tannor Breitigam <70126993+breizeway@users.noreply.github.com>
2024-01-07 11:47:58 -0800

    Update index.tsx

--- client/src/components/banner-ad/index.tsx
+++ client/src/components/banner-ad/index.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import { useMemo } from "react";
 import ScalingImage from "../scaling-image";
 
 const NUM_ADS = 32;
 export default function BannerAd() {
-  const src = `/ads/ad_${Math.round(Math.random() * (NUM_ADS - 1))}.gif`;
+  const src = useMemo(() => `/ads/ad_${Math.round(Math.random() * (NUM_ADS - 1))}.gif`, []);
   return (
     <a href="https://tannor.net" target="_blank" className="w-fit">
       <ScalingImage alt="banner ad" {...{ src }} />
