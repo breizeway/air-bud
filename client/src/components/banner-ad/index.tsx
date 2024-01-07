e0b29b7de72c2e86f8183a08e516fc65c7d82ee8
Tannor Breitigam <70126993+breizeway@users.noreply.github.com>
2024-01-06 16:26:21 -0800

    Update index.tsx

--- client/src/components/banner-ad/index.tsx
+++ client/src/components/banner-ad/index.tsx
@@ -5,7 +5,7 @@ export default function BannerAd() {
   const src = `/ads/ad_${Math.round(Math.random() * (NUM_ADS - 1))}.gif`;
   return (
     <a href="https://tannor.net" target="_blank" className="w-fit">
-      <ScalingImage alt="banner ad" {...{ src }} />;
+      <ScalingImage alt="banner ad" {...{ src }} />
     </a>
   );
 }
